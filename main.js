4b7c6da7c1af9a828467e49ee9b49c15baaa1e97
Henrique <134407416+inshenrique@users.noreply.github.com>
2023-05-29 15:52:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-var playlistId = "u9KHrtQEIZ0";
-var videoId = "pJdUJmSqDGE";
 var player;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
+        height: '360',
+        width: '640',
         playerVars: {
-            'autoplay': 1,
-            'loop': 1,
-            'playlist': playlistId
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            'autoplay': 1,         // Inicia o vídeo automaticamente
+            'loop': 1,             // Reproduz a playlist em looping
+            'playlist': 'u9KHrtQEIZ0'  // ID da playlist
         }
     });
-}
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-function onPlayerStateChange(event) {
-    if (event.data === YT.PlayerState.ENDED && event.target.getVideoData().video_id === videoId) {
-        player.loadPlaylist(playlistId);
-    }
 }
